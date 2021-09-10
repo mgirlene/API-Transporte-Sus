f79da2c66d3769d28aad9bd7f54dad10952f7285
mgirlene <mgirlene.silva0908@gmail.com>
2021-09-10 11:56:10 -0300

    Configuracao Heroku

--- API_TransporteSus/settings.py
+++ API_TransporteSus/settings.py
@@ -1,6 +1,7 @@
 
 from datetime import timedelta
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,6 +137,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
 }
+
+django_heroku.settings(locals())
