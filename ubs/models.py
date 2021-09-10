b42ad340e6874248b9302a6bdb378aa34a4493d0
neudinho11 <neudinho66@gmail.com>
2021-09-10 20:17:06 -0300

    Alteração no tipo do field

--- ubs/models.py
+++ ubs/models.py
@@ -4,8 +4,8 @@ from django.db.models.deletion import CASCADE
 
 class Estado(models.Model):
     id = models.AutoField(primary_key=True,db_column="id_estado")
-    nome = models.TextField('nome', max_length=50)
-    sigla = models.TextField('sigla', max_length=2)
+    nome = models.CharField('nome', max_length=50)
+    sigla = models.CharField('sigla', max_length=2)
 
     def __str__(self):
         return '{}'.format(self.nome)
@@ -17,7 +17,7 @@ class Estado(models.Model):
 
 class Cidade(models.Model):
     id = models.AutoField(primary_key=True,db_column="id_cidade")
-    nome = models.TextField(max_length=50)
+    nome = models.CharField(max_length=50)
     estado = models.ForeignKey(Estado, on_delete= models.CASCADE)
 
     def __str__(self):
@@ -30,7 +30,7 @@ class Cidade(models.Model):
 
 class Ubs(models.Model):
     id = models.AutoField(primary_key=True,db_column="id_ubs")
-    nome = models.TextField(max_length=50)
+    nome = models.CharField(max_length=50)
     cidade = models.ForeignKey(Cidade, on_delete= models.CASCADE)
     endereco = models.CharField(max_length=255)
     cnes = models.CharField(max_length=20)
